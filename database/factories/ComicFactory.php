03f34b286c6384fcfc35b72bab9e27f028bd507c
Jack-170 <agavejack@hotmail>
2024-02-15 15:58:04 +0100

    create comics table in db

---
+++ database/factories/ComicFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Comic>
+ */
+class ComicFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'title' => fake()->sentence(),
+            'price' => fake()->randomFloat(2, 10, 100),
+            'release_date' => fake()->date(),
+
+        ];
+    }
+}
